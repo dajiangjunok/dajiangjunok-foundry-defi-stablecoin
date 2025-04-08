74bb37d63042431a08cf55623e03b3281b7b352a
Alivin <wangyongjie@mediinfo.com.cn>
2025-04-08 20:49:45 +0800

    fix(test): 优化测试文件
    提高代码可读性

--- test/fuzz/OpenInvariantsTest.t.sol
+++ test/fuzz/OpenInvariantsTest.t.sol
@@ -1,10 +1,7 @@
 // Hove our invariant aka properties
 // What are our invariants?
-
 // 1.DSC 总供应量应该小于质押品的总价值
-
 // 2. getter view function should never revert <- evergreen invariant
-
 pragma solidity ^0.8.26;
 
 import {Test, console} from "forge-std/Test.sol";
